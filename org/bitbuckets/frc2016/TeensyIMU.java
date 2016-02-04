bb0e85853c02aedb477dbd37b75ea63c1b154dca
Sumeet Chandra <scsumeetchandra@gmail.com>
2016-02-04 16:52:46 -0700

    Added return methods for gyro, pose, etc. from raw data parsing

--- org/bitbuckets/frc2016/TeensyIMU.java
+++ org/bitbuckets/frc2016/TeensyIMU.java
@@ -1,5 +1,7 @@
 package org.bitbuckets.frc2016;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.nio.ByteBuffer;
 
 //import edu.wpi.first.wpilibj.SPI;
@@ -10,12 +12,15 @@ import jssc.SerialPortList;
 
 public class TeensyIMU {
 	
-	public ByteBuffer readBuffer = ByteBuffer.allocateDirect(1024);
-	public ByteBuffer writeBuffer = ByteBuffer.allocateDirect(1024);	
+	private ByteBuffer readBuffer = ByteBuffer.allocateDirect(1024);
+	private ByteBuffer writeBuffer = ByteBuffer.allocateDirect(1024);
+	private SerialPort serialPort;
+	private String RAW;
+	private String[]lines;
 	
 	public TeensyIMU(){
 				
-		final SerialPort serialPort = new SerialPort(SerialPortList.getPortNames()[0]);
+		serialPort = new SerialPort(SerialPortList.getPortNames()[0]);
 	    
 	    try {
 	        serialPort.openPort();//Open serial port
@@ -33,8 +38,10 @@ public class TeensyIMU {
 				// TODO Auto-generated method stub
 				while(true){
 						try {
-							//System.out.println(serialPort.readString());
-							System.out.println(serialPort.readHexString());
+							
+							RAW = serialPort.readString();
+							lines = RAW.split(System.getProperty("line.separator"));
+							//System.out.println(serialPort.readHexString());
 							//serialPort.closePort();
 						} catch (SerialPortException e) {
 							// TODO Auto-generated catch block
@@ -48,7 +55,43 @@ public class TeensyIMU {
 						}			                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
 			}
 			}).start();
+		 
 		}
+	
+	public float getGyro(){
+		
+		if(!serialPort.isOpened())
+		return 0;
+		else{
+			return hexToDouble(lines[0]);
+		}
+		
+	}
+	
+	public float getAccel(){
+		
+		if(!serialPort.isOpened())
+			return 0;
+			else{
+				return hexToDouble(lines[1]);
+			}
+	}
+	
+	public float getMag(){
+		if(!serialPort.isOpened())
+			return 0;
+			else{
+				return hexToDouble(lines[2]);
+			}
+	}
+	
+	public float getPose(){
+		if(!serialPort.isOpened())
+			return 0;
+			else{
+				return hexToDouble(lines[3]);
+			}
+	}
 /**It's the port we deserve, but not the port we need right now
 public SPI spi;
 		spi = new SPI(port);
@@ -76,6 +119,22 @@ public SPI spi;
 			}
 			
 		}).start();
-*/	
+*/
+
+	private float hexToDouble(String str){
+
+        //Parses string as decimal
+        Long i = Long.parseLong(str, 16);
+        //Converts newly created decimals to floating point
+        Float f = Float.intBitsToFloat(i.intValue());
+
+        while(f<(float)4294967295.0) {
+            return f;
+        }
+        //Prints original hex number
+        //System.out.println(Integer.toHexString(Float.floatToIntBits(f)));
+        //Integer.parseInt("099FA", 16);
+        return 0;
+    }	
 }
 	
